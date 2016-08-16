72631a44a26172f581cd5fc23595c889c8e63875
Maximilian Meffert <maxmeffert@uni-koblenz.de>
2016-08-16 10:54:36 +0200

    added sami config

---
+++ samicfg.php
@@ -0,0 +1,16 @@
+<?php
+
+include_once __DIR__."/vendor/autoload.php";
+
+$src = __DIR__."/src";
+$out = __DIR__."/docs";
+$csh = __DIR__."/cache";
+
+$options = [
+		"build_dir" => $out,
+		"cache_dir" => $csh,
+];
+
+return new Sami\Sami($src,$options);
+
+?>
