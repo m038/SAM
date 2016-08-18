e1c3c820877aacf0b65354a13dcca4b10457dc2f
Maximilian Meffert <maxmeffert@uni-koblenz.de>
2016-08-18 02:34:13 +0200

    refined AppTest

--- tests/AppTest.php
+++ tests/AppTest.php
@@ -4,14 +4,24 @@ use PHPUnit\Framework\TestCase;
 
 use intrawarez\slim3annotations\annotations\Group;
 use intrawarez\slim3annotations\annotations\GET;
+use intrawarez\slim3annotations\annotations\POST;
+use intrawarez\slim3annotations\annotations\PUT;
+use intrawarez\slim3annotations\annotations\DELETE;
 use intrawarez\slim3annotations\App;
-use Slim\Interfaces\RouteGroupInterface;
-use Slim\Interfaces\RouteInterface;
-use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
-use Slim\Http\Request;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\Http\Body;
 use Slim\Http\Environment;
+use Slim\Http\Request;
 use Slim\Http\Response;
+use Slim\Interfaces\RouteInterface;
+use Slim\Interfaces\RouterInterface;
+
+const METHOD1 = 661;
+const METHOD2 = 662;
+const METHOD3 = 663;
+const METHOD4 = 664;
+const METHOD5 = 665;
 
 /**
  * 
@@ -25,7 +35,9 @@ class DummyController {
 	 */
 	public function method1 (ServerRequestInterface $req, ResponseInterface $res, array $args) : ResponseInterface {
 		
-		return $res->withStatus(200);
+		$res->getBody()->write(METHOD1);
+				
+		return $res;
 		
 	}
 	
@@ -35,7 +47,45 @@ class DummyController {
 	public function method2 (ServerRequestInterface $req, ResponseInterface $res, array $args) : ResponseInterface {
 		
 
-		return $res->withStatus(404);
+		$res->getBody()->write(METHOD2);
+		
+		return $res;
+		
+	}
+	
+	/**
+	 * @POST
+	 */
+	public function method3 (ServerRequestInterface $req, ResponseInterface $res, array $args) : ResponseInterface {
+		
+
+		$res->getBody()->write(METHOD3);
+		
+		return $res;
+		
+	}
+	
+	/**
+	 * @PUT
+	 */
+	public function method4 (ServerRequestInterface $req, ResponseInterface $res, array $args) : ResponseInterface {
+		
+
+		$res->getBody()->write(METHOD4);
+		
+		return $res;
+		
+	}
+	
+	/**
+	 * @DELETE
+	 */
+	public function method5 (ServerRequestInterface $req, ResponseInterface $res, array $args) : ResponseInterface {
+		
+
+		$res->getBody()->write(METHOD5);
+		
+		return $res;
 		
 	}
 	
@@ -43,43 +93,87 @@ class DummyController {
 
 class AppTest extends TestCase {
 	
-	public function testLoad () {
+	static private function newAppRouter () : RouterInterface {
 		
 		$app = new App();
+		$app->load(DummyController::class);
 		
-		$group = $app->load(DummyController::class);
+		return $app->getContainer()->get("router");
 		
-		$this->assertInstanceOf(RouteGroupInterface::class, $group);
+	}
+	
+	static private function newRequest () : ServerRequestInterface {
 		
-
-		$request = Request::createFromEnvironment(Environment::mock());
-		$response = new Response();
+		return Request::createFromEnvironment(Environment::mock());
 		
-		/**
-		 * 
-		 * @var \Slim\Router $router
-		 */
-		$router = $app->getContainer()->get("router");
+	}
+	
+	static private function newResponse () : ResponseInterface {
 		
-		// rotue0 -> method1
+		return new Response();
+		
+	}
+	
+	private function assertRoute (RouteInterface $route, $expectedMethods, $expectedPattern, $expectedContents) {
+		
+		$this->assertAttributeContains($expectedMethods, "methods", $route);
+		$this->assertEquals($expectedPattern, $route->getPattern());
+		
+		$response = $route->__invoke(self::newRequest(), self::newResponse());
+		
+		$body = $response->getBody();
+		$body->rewind();
+		$this->assertEquals($expectedContents, $body->getContents());
+		
+	}
+	
+	public function testMethod1 () {
+		
+		$router = self::newAppRouter();
 		
 		$route = $router->lookupRoute("route0");
-		$response = $route->__invoke($request, $response);
 		
-		$this->assertInstanceOf(RouteInterface::class, $route);
-		$this->assertAttributeContains("GET", "methods", $route);
-		$this->assertEquals("/dummy", $route->getPattern());
-		$this->assertEquals(200, $response->getStatusCode());
+		$this->assertRoute($route, "GET", "/dummy", METHOD1);
 		
-		// route1 -> method2
+	}
+	
+	public function testMethod2 () {
+		
+		$router = self::newAppRouter();
 		
 		$route = $router->lookupRoute("route1");
-		$response = $route->__invoke($request, $response);
 		
-		$this->assertInstanceOf(RouteInterface::class, $route);
-		$this->assertAttributeContains("GET", "methods", $route);
-		$this->assertEquals("/dummy/foo", $route->getPattern());
-		$this->assertEquals(404, $response->getStatusCode());
+		$this->assertRoute($route, "GET", "/dummy/foo", METHOD2);
+		
+	}
+	
+	public function testMethod3 () {
+		
+		$router = self::newAppRouter();
+		
+		$route = $router->lookupRoute("route2");
+		
+		$this->assertRoute($route, "POST", "/dummy", METHOD3);
+		
+	}
+	
+	public function testMethod4 () {
+		
+		$router = self::newAppRouter();
+		
+		$route = $router->lookupRoute("route3");
+		
+		$this->assertRoute($route, "PUT", "/dummy", METHOD4);
+		
+	}
+	
+	public function testMethod5 () {
+		
+		$router = self::newAppRouter();
+		
+		$route = $router->lookupRoute("route4");
+		
+		$this->assertRoute($route, "DELETE", "/dummy", METHOD5);
 		
 	}
 	
