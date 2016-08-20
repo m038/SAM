a59db69c668bc540a980f9eda3eb7d3444c6c1f7
Maximilian Meffert <maxmeffert@uni-koblenz.de>
2016-08-20 03:39:01 +0200

    removed master files

--- docs.sh
+++ docs.sh
@@ -1,4 +1,15 @@
-sudo rm -r docs
-php ./vendor/sami/sami/sami.php update samicfg.php
-rm -r ./cache
-
+git checkout master ;
+echo "\nCopying: src -> .src\n" ;
+cp -r src .src ;
+git checkout docs ;
+echo "\nMoving: .src -> src\n" ;
+mv .src src ;
+if [ ! -f sami.phar ]; then
+	echo "\nDownloading: http://get.sensiolabs.org/sami.phar\n" ;
+	wget http://get.sensiolabs.org/sami.phar ;
+fi
+php sami.phar update samicfg.php ;
+echo "\nRemoving: cache\n" ;
+rm -r cache ;
+echo "\nRemoving: src\n" ;
+rm -r src ;
--- samicfg.php
+++ samicfg.php
@@ -1,7 +1,5 @@
 <?php
 
-include_once __DIR__."/vendor/autoload.php";
-
 $src = __DIR__."/src";
 $out = __DIR__."/docs";
 $csh = __DIR__."/cache";
