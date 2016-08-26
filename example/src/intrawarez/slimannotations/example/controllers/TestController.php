96e393ad85762120c4f34ad22a243ecb2b94d855
Maximilian Meffert <maxmeffert@uni-koblenz.de>
2016-08-26 08:25:53 +0200

    enforced psr2 code style

--- example/src/intrawarez/slimannotations/example/controllers/TestController.php
+++ example/src/intrawarez/slimannotations/example/controllers/TestController.php
@@ -1,10 +1,8 @@
 <?php
-
 namespace intrawarez\slimannotations\example\controllers;
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
-
 use intrawarez\slim3annotations\annotations\Group;
 use intrawarez\slim3annotations\annotations\GET;
 use intrawarez\slim3annotations\annotations\Dependency;
@@ -12,47 +10,36 @@ use Interop\Container\ContainerInterface;
 
 /**
  * @Group(pattern="/")
- * 
- * 
+ *
  * @author maxmeffert
- * 
- * 
- * 
  */
-class TestController {
-	
-	/**
-	 * @Dependency(id="twig")
-	 * @var \Twig_Environment
-	 * 
-	 */
-	private $twig;
-	
-	
-	/**
-	 * @GET
-	 * 
-	 * @param ServerRequestInterface $req
-	 * @param ResponseInterface $res
-	 * @param array $args
-	 * 
-	 * 
-	 */
-	public function fii (ServerRequestInterface $req, ResponseInterface $res, array $args) {
-// 		var_dump($this->twig);
-// 		throw new \Exception("lala");
-		
-		$twig = $this->twig;
-	
-		$res->getBody()->write($twig->render("index.twig",[
-				"hello" => "World"
-		]));
-		
-		return $res;
-		
-	}
-	
+class TestController
+{
+
+    /**
+     * @Dependency(id="twig")
+     *
+     * @var \Twig_Environment
+     */
+    private $twig;
+
+    /**
+     * @GET
+     *
+     * @param ServerRequestInterface $req
+     * @param ResponseInterface $res
+     * @param array $args
+     */
+    public function fii(ServerRequestInterface $req, ResponseInterface $res, array $args)
+    {
+        // var_dump($this->twig);
+        // throw new \Exception("lala");
+        $twig = $this->twig;
+        
+        $res->getBody()->write($twig->render("index.twig", [
+            "hello" => "World"
+        ]));
+        
+        return $res;
+    }
 }
-
-
-?>
