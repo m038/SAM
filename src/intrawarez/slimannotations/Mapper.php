467b7fb26ee080ff85823d6a5de2bcb157296c81
Maximilian Meffert <maxmeffert@uni-koblenz.de>
2016-10-12 02:12:39 +0200

    fixed psr2 formatting

--- src/intrawarez/slimannotations/Mapper.php
+++ src/intrawarez/slimannotations/Mapper.php
@@ -4,6 +4,7 @@ namespace intrawarez\slimannotations;
 use intrawarez\slimannotations\metadata\ClassMetadata;
 use Slim\App;
 use intrawarez\slimannotations\delegates\GroupDelegate;
+use intrawarez\slimannotations\delegates\ActionDelegate;
 
 class Mapper
 {
@@ -20,6 +21,11 @@ class Mapper
     {
         if ($classMetadata->isGroupDeclaration()) {
             $this->app->group($classMetadata->getGroupDeclaration()->pattern, new GroupDelegate($this->app, $this->instantiator, $classMetadata));
+        } elseif ($classMetadata->isActionDeclaration()) {
+            $httpMethod = $classMetadata->getActionDeclaration();
+            if ($httpMethod instanceof GET) {
+                $this->getApp()->get($httpMethod->pattern, new ActionDelegate($this->app, $this->instantiator, $classMetadata));
+            }
         }
     }
 }
