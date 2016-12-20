5f88eac3dbd2cb461b91e1192284d90ea3bfd1fd
Mischa Gorinskat <m.gorinskat@gmail.com>
2016-12-20 17:32:24 +0100

    Fixes merge remains in console file

--- src/intrawarez/sam/cli/SAMConsole.php
+++ src/intrawarez/sam/cli/SAMConsole.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace intrawarez\sam\cli;
 
 use Symfony\Component\Console\Application;
@@ -7,25 +8,13 @@ use intrawarez\sam\cli\commands\Compile;
 
 final class SAMConsole extends Application
 {
-<<<<<<< HEAD
-<<<<<<< HEAD
     const NAME = "SAM";
     const VERSION = "1.0.0";
-    
+
     public function __construct()
     {
         parent::__construct(self::NAME, self::VERSION);
-=======
-    public function __construct()
-    {
-        parent::__construct("sam", "1.0.0");
->>>>>>> 77690e8104c4d1850bada6375323f7d89848d8db
-=======
-    public function __construct()
-    {
-        parent::__construct("sam", "1.0.0");
->>>>>>> 77690e8104c4d1850bada6375323f7d89848d8db
-        
+
         $this->add(new Validate());
         $this->add(new Compile());
     }
